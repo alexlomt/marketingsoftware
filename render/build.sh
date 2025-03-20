8ba105f450c3899a212b1ac766bad3053a086f96
alexlomt <alexlomt@yahoo.com>
2025-03-20 20:12:05 +0000

    Update build.sh

--- render/build.sh
+++ render/build.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 set -e
 # Render Build Script
-# This script is executed by Render during the build phase
 
 # Install dependencies
 npm install
 
 # Install TypeScript dependencies explicitly
-npm install --save-dev typescript @types/node
+npm install --save-dev typescript @types/node @types/react @types/react-dom
 
 # Build the Next.js application
 npm run build
