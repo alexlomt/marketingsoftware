14d0b015ee2bd3f2159bd6de5bd5059aecdc9f63
alexlomt <alexlomt@yahoo.com>
2025-03-24 01:17:44 +0000

    Delete static directory

--- static/chunks/app/dashboard/page-7238c41a70f85792.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[702,64,613,772,744,743,409,462],{3521:function(){}},function(n){n.O(0,[484,560],function(){return n(n.s=3521)}),_N_E=n.O()}]);
--- static/chunks/app/deals/page-a9929b79478fe758.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[772,64,613,702,744,743,409,462],{3521:function(){}},function(n){n.O(0,[484,560],function(){return n(n.s=3521)}),_N_E=n.O()}]);
