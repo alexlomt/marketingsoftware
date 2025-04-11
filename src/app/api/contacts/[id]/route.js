cf25e0eb1bb625d74b10a901f04626ebbc172d8b
alexlomt <alexlomt@yahoo.com>
2025-04-11 12:32:30 +0100

    Update route.js

--- src/app/api/contacts/[id]/route.js
+++ src/app/api/contacts/[id]/route.js
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import { NextResponse } from 'next/server';
 import { getContactById, updateContact, deleteContact } from '@/models/contact';
 import { getOrganizationId } from '@/lib/auth-utils';
