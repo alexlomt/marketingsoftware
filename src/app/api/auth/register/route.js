466896063d7519ab933c020bfa486fbae08c5eef
alexlomt <alexlomt@yahoo.com>
2025-04-11 12:31:51 +0100

    Update route.js

--- src/app/api/auth/register/route.js
+++ src/app/api/auth/register/route.js
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import { NextResponse } from 'next/server';
 import { createUser } from '@/models/user';
 import { createOrganization } from '@/models/organization';
