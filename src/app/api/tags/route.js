f7b424c0ee38ebc2db4dac6ee2d8a0646d004551
alexlomt <alexlomt@yahoo.com>
2025-04-11 12:40:28 +0100

    Update route.js

--- src/app/api/tags/route.js
+++ src/app/api/tags/route.js
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import { NextResponse } from 'next/server';
 import { createTag, getTagsByOrganization } from '@/models/tag';
 import { getOrganizationId } from '@/lib/auth-utils';
