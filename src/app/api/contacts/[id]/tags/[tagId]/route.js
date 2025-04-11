a272dc91d8327442315374962d95636c45e10ef2
alexlomt <alexlomt@yahoo.com>
2025-04-11 12:32:43 +0100

    Update route.js

--- src/app/api/contacts/[id]/tags/[tagId]/route.js
+++ src/app/api/contacts/[id]/tags/[tagId]/route.js
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import { NextResponse } from 'next/server';
 import { removeTagFromContact } from '@/models/tag';
 import { getOrganizationId } from '@/lib/auth-utils';
