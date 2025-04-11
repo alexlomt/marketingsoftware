58302c8b949a09ac73f0094c918631463f7f4610
alexlomt <alexlomt@yahoo.com>
2025-04-11 12:26:39 +0100

    Update route.js

--- src/app/api/appointments/[id]/route.js
+++ src/app/api/appointments/[id]/route.js
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import { NextResponse } from 'next/server';
 import { getAppointmentById, updateAppointment, deleteAppointment } from '@/models/appointment';
 import { getOrganizationId } from '@/lib/auth-utils';
