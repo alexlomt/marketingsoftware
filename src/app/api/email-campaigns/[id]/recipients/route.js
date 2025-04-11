81cbec4363ab32e0cb28a1618bd127c26d3404ce
alexlomt <alexlomt@yahoo.com>
2025-04-11 12:35:13 +0100

    Update route.js

--- src/app/api/email-campaigns/[id]/recipients/route.js
+++ src/app/api/email-campaigns/[id]/recipients/route.js
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import { NextResponse } from 'next/server';
 import { getCampaignRecipients, getCampaignStatistics, updateRecipientStatus } from '@/models/emailCampaign';
 import { getOrganizationId } from '@/lib/auth-utils';
